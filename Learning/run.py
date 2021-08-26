70a029a3cbc545a217409d049ecdba85fff71848
JiyongAhn_Laptop <ajy8456@gmail.com>
2021-08-26 15:08:05 +0900

    load checkpoint

--- Learning/run.py
+++ Learning/run.py
@@ -10,7 +10,7 @@ from load import get_loader
 from model import GPT2
 from trainer import Trainer
 from evaluator import Evaluator
-from saver import save_checkpoint
+from saver import save_checkpoint, load_checkpoint
 
 
 @dataclass
@@ -42,6 +42,7 @@ class Settings(Serializable):
     # Training
     device: str = 'cuda' if th.cuda.is_available() else 'cpu'
     # device: str = 'cpu'
+    resume: str = None # checkpoint file name for resuming
     # |NOTE| Large # of epochs by default, Such that the tranining would *generally* terminate due to `train_steps`.
     epochs: int = 10000
     learning_rate: float = 1e-7
@@ -50,11 +51,11 @@ class Settings(Serializable):
 
     # Logging
     exp_dir: str = 'Learning/exp'
-    model_name: str = '8.25_lr_scheduler_1e-3_to_le-7_AdamW'
+    model_name: str = '8.26_lr_scheduler_1e-3_to_le-7_AdamW_clip'
     print_freq: int = 1000 # per train_steps
     train_eval_freq: int = 1000 # per train_steps
     test_eval_freq: int = 1 # per epochs
-    save_freq: int = 20 # per epochs
+    save_freq: int = 1000 # per epochs
 
 
 def main():
@@ -104,8 +105,20 @@ def main():
                           model=model,
                           eval_fn=eval_fn)
 
+    start_epoch = 1
     best_error = 10000.
-    for epoch in range(1, config.epochs+1):
+
+    # load checkpoint for resuming
+    if config.resume is not None:
+        if os.path.isfile(os.path.join(model_dir, config.resume)):
+            start_epoch, best_error, model, optimizer = load_checkpoint(model, optimizer, scheduler, config.resume,)
+            start_epoch += 1
+            print("Loaded checkpoint '{}' (epoch {})".format(config.resume, start_epoch))
+        else:
+            print("No checkpoint found at '{}'".format(config.resume))
+
+
+    for epoch in range(start_epoch, config.epochs+1):
         print(f'===== Start {epoch} epoch =====')
         
         # Training one epoch
@@ -123,16 +136,26 @@ def main():
             if test_val < best_error:
                 best_error = test_val
 
-                save_checkpoint(epoch, model, optimizer, best_error,
+                save_checkpoint('Saving the best model!',
                                 os.path.join(model_dir, 'best.pth'),
-                                'Saving the best model!')
+                                epoch, 
+                                best_error, 
+                                model, 
+                                optimizer, 
+                                scheduler
+                                )
             logger.add_scalar('Eval/test', test_val, epoch)
         
         # save the model
         if epoch % config.save_freq == 0:
-            save_checkpoint(epoch, model, optimizer, test_val,
-                            os.path.join(model_dir, f'ckpt_epoch_{epoch}.pth'),
-                            'Saving...')
+            save_checkpoint('Saving...', 
+                            os.path.join(model_dir, f'ckpt_epoch_{epoch}.pth'), 
+                            epoch, 
+                            test_val, 
+                            model, 
+                            optimizer, 
+                            scheduler
+                            )
 
         print(f'===== End {epoch} epoch =====')
 
--- Learning/saver.py
+++ Learning/saver.py
@@ -1,21 +1,27 @@
 import torch as th
 
 
-def save_checkpoint(epoch, model, optimizer, val, filename, msg):
+def save_checkpoint(msg, filename, epoch, val, model, optimizer, scheduler=None):
     state = {
         'epoch': epoch,
         'state_dict': model.state_dict(),
         'optimizer': optimizer.state_dict(),
         'top_eval': val
     }
+    if scheduler is not None:
+        state['scheduler'] = scheduler.state_dict()
+    
     th.save(state, filename)
     print(msg)
 
 
-def load_checkpoint(model, optimizer, filename):
+def load_checkpoint(model, optimizer, filename, scheduler=None):
     checkpoint = th.load(filename, map_location='cuda:0')
     start_epoch = checkpoint['epoch']
+    val = checkpoint['top_eval']
     model.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
+    if scheduler is not None:
+        scheduler.load_state_dict(checkpoint['scheduler'])
 
-    return start_epoch, model, optimizer
+    return start_epoch, val, model, optimizer, scheduler
